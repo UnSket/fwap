750a7e0abb5ea30b30e605d67fd916698d988c8b
Vlad <v.tsvetkov@pflb.com>
2019-07-01 22:02:42 +0300

    fixes, info part

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import styles from './Header.module.scss';
 import logo from './logo_white.svg';
 import { HashLink } from 'react-router-hash-link';
-import { AppBar, IconButton, Toolbar, Button, SwipeableDrawer } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, IconButton, Toolbar, Button, Container } from '@material-ui/core';
+//import MenuIcon from '@material-ui/icons/Menu';
 import Hidden from '@material-ui/core/Hidden';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -18,12 +18,13 @@ const Header: React.FC = () => {
     return (
         <>
             <AppBar position="static">
-                <Toolbar className={styles.container}>
+                <Toolbar className={styles.header}>
+                    <Container className={styles.container}>
                     <div className={styles.leftPart}>
                         <Hidden smUp>
                         <div className={styles.toggleMenu}>
                             <IconButton onClick={openMenu}>
-                                <MenuIcon className={styles.toggleMenu} />
+                              {/*<MenuIcon className={styles.toggleMenu} />*/}
                             </IconButton>
                         </div>
                         </Hidden>
@@ -42,6 +43,7 @@ const Header: React.FC = () => {
                     <div className={styles.leftPart}>
                         <Button color="inherit">Login</Button>
                     </div>
+                    </Container>
                     <Menu
                         id="simple-menu"
                         anchorEl={anchorForMenu}
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import styles from './Landing.module.scss';
-import {Typography} from '@material-ui/core';
+import {Typography, Container} from '@material-ui/core';
+import gearsImg from './gears.png';
 
 const Landing: React.FC = () => {
   return (
-      <div className={styles.container}>
+    <div className={styles.mainContent}>
+      <section className={styles.intro}>
         <div className={styles.content}>
-          <Typography variant={'h1'}>FWAP</Typography>
-          <Typography variant={'h3'}>IT'S COOL TOOL THAT YOU MUST HAVE</Typography>
+          <Container className={styles.container}>
+            <Typography variant={'h1'}>FWAP</Typography>
+            <Typography variant={'h3'}>THIS IS TOOL WHICH WILL HELP YOU TO LEARN LANGUAGES FUN</Typography>
+          </Container>
         </div>
-      </div>
+      </section>
+      <section className={styles.whatIsIt}>
+        <img className={styles.gears} src={gearsImg} />
+        <div className={styles.content}>
+          <Container className={styles.container}>
+            <Typography variant={'h2'}>How it works?</Typography>
+            <Typography variant={'h4'} component={'p'}>
+              Текст (от лат. textus — ткань; сплетение, сочетание) — зафиксированная на каком-либо материальном носителе человеческая мысль; в общем плане связная и полная последовательность символов.
+              Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и репрезентативная (более частная)
+            </Typography>
+          </Container>
+        </div>
+      </section>
+    </div>
   );
 };
 
