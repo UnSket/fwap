73d78c72605078e962062a472c8368d0500d2944
vlad-tsvetkov <vlad-tsvetkov@yandex-team.ru>
2019-07-01 10:12:09 +0300

    fixes style, landing first section

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './Landing.module.scss';
+import {Typography} from '@material-ui/core';
 
 const Landing: React.FC = () => {
-  return <b>landing</b>;
+  return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Typography variant={'h1'}>FWAP</Typography>
+          <Typography variant={'h3'}>IT'S COOL TOOL THAT YOU MUST HAVE</Typography>
+        </div>
+      </div>
+  );
 };
 
 export default Landing;
